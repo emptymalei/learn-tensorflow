0d9ea377a3d88fc21629dcaf9a26aa28e0fdcb03
OctoMiao <emptymalei@qq.com>
2017-04-11 17:36:16 -0600

    a more explicit model of differential equation but it doens't work

---
+++ eqn/tfde/__init__.py
@@ -0,0 +1,21 @@
+"""
+TFDE: Python Package for Neutrino Oscillations using Tensorflow
+=================================================================
+
+Documentation is available in the docstrings and
+online at https://neuosc.neutrino.xyz.
+
+Contents
+--------
+
+This module is till WIP. The plan is to cover several subpackages:
+
+::
+
+  tfde                      --- a file
+
+To use NeuOsc subpackages, simply import the correspoding subpackage,
+e.g., ``import neuosc.constants``.
+
+"""
+__all__=["tfde"]
